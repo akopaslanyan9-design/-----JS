157e053d10ec7dc5a52076cbb784905fcd4ffdb5
artos <akopslanyan9@gmail.com>
2026-07-12 13:08:50 +0300

    и

--- 4-навигатор/index.js
+++ 4-навигатор/index.js
@@ -1,11 +1,9 @@
-let addressLat = 10;      //positionLatitude 	 Широта текущего положения 
-let addressLong  = 20;    // positionLongitude 	 Долгота текущего положения 
-let addressLatitude = 2;  // addressLatitude 	 Широта пункта назначения 
-let addressLongitude = 5; // addressLongitude 	 Долгота пункта назначения 
-let Latitude = (addressLat ** 2) + (addressLatitude ** 2);
-let result = Math.sqrt(Latitude);
-let Longitude = (addressLong ** 2) + (addressLongitude ** 2);
-let result2 = Math.sqrt(Longitude);
-console.log(result);//результат
-console.log(result2);//результат
-
+let positionLatitude = 10;     
+let positionLongitude = 20;    
+let addressLatitude = 2;    
+let addressLongitude = 2;     
+let diffLat = positionLatitude - addressLatitude; 
+let diffLong = positionLongitude - addressLongitude; 
+let sumOfSquares = (diffLat ** 2) + (diffLong ** 2);
+let distance = Math.sqrt(sumOfSquares);
+console.log("Рассчитанное расстояние:", distance);
