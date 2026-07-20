3294563582d6d002aa29a991098529eda30277a4
artos <akopslanyan9@gmail.com>
2026-07-20 22:02:30 +0300

    dz

---
+++ 6-driver/driver.js
@@ -0,0 +1,11 @@
+const hasLicence = true;
+const age = Number(prompt("Введите ваш возраст"));
+const isDrunk = false;
+
+if (age >= 18  
+   && hasLicence 
+   && isDrunk) {
+    console.log('может');
+   }else {
+    console.log('не может');
+   }
